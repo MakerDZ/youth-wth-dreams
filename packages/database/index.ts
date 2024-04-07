484ecb7fa26e59fcd02785eb7dba1b8353a8f751
MakerDZ <mcskb91@gmail.com>
2024-04-07 18:18:06 +0630

    :construction: wip(database): improved database schema design for the current MVP.

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,6 +1,4 @@
 export * from './libs/database';
 export * from './models/identity.model';
 export * from './models/entrance.model';
-export * from './models/dreams.model';
-export * from './models/commitments.model';
-export * from './models/emotions.model';
+export * from './models/notes.model';
--- packages/database/models/notes.model.ts
+++ packages/database/models/notes.model.ts
@@ -1,7 +1,20 @@
 import mongoose from 'mongoose';
 
+enum NoteType {
+    dream = 'dream',
+    commitment = 'commitment',
+    emotion = 'emotion',
+    showcase = 'showcase',
+    execution = 'execution',
+    struggle = 'struggle',
+    setback = 'setback',
+    short_term_target = 'short_term_target',
+    this_year_target = 'this_year_target',
+}
+
 interface TypeDreams extends Document {
     accountId: string;
+    type: NoteType;
     note: string;
     attachment: string[];
     isAnonymous: boolean;
@@ -12,6 +25,11 @@ interface TypeDreams extends Document {
 const dreamSchema: mongoose.Schema<TypeDreams> = new mongoose.Schema(
     {
         accountId: { type: String, unique: true, required: true },
+        type: {
+            type: String,
+            enum: Object.values(NoteType).map((value) => value.toString()),
+            required: true,
+        },
         note: { type: String, unique: true, required: true },
         attachment: [{ type: String, required: false }],
         isAnonymous: { type: Boolean, required: true },
